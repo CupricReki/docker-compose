fbfcebf1f320cfe2ca72e6a4d4dc1d3b739997c5
CupricReki <CupricReki@gmail.com>
2019-09-16 13:05:27 -0600

    added traefik

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-mkdir /opt/dl/sonarr /opt/dl/radarr /opt/dl/transmission /opt/dl/sabnzbd /opt/dl/portainer /opt/dl/watchtower
+mkdir /opt/dl/sonarr /opt/dl/radarr /opt/dl/transmission /opt/dl/sabnzbd /opt/dl/portainer /opt/dl/watchtower /opt/dl/traefik
+touch /opt/dl/traefik/acme.json
+chmod 600 /opt/dl/traefik/acme.json
+cp $PWD/traefik/traefik.toml /opt/dl/traefik/
 chown 1007:1008 -R /opt/dl/*
