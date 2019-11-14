a61044356cfc6f317b927ec6bbd863c8af78ef73
Skyler Ogden <cupricreki@gmail.com>
2019-11-14 00:52:51 -0700

    added unifi-controller

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /opt/dl/sonarr /opt/dl/radarr /opt/dl/transmission /opt/dl/sabnzbd /opt/dl/portainer /opt/dl/watchtower /opt/dl/traefik
+mkdir /opt/dl/unifi-controller /opt/dl/sonarr /opt/dl/radarr /opt/dl/transmission /opt/dl/sabnzbd /opt/dl/portainer /opt/dl/watchtower /opt/dl/traefik
 touch /opt/dl/traefik/acme.json
 chmod 600 /opt/dl/traefik/acme.json
 cp $PWD/traefik/traefik.toml /opt/dl/traefik/
